d2bdbe820eef8c00c8d7c4ab0494bd2fa6b50891
akhpkn <akh.andr@mail.ru>
2020-01-23 21:30:53 +0300

    modified security config

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -83,7 +83,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .antMatchers("/api/user/checkUsernameAvailability", "/api/user/checkEmailAvailability")
                 .permitAll()
-                .antMatchers(HttpMethod.GET, "/api/polls/**", "/api/users/**")
+                .antMatchers(HttpMethod.GET, "/api/courses/**", "/api/users/**")
+                .permitAll()
+                .antMatchers(HttpMethod.POST, "/api/courses")
                 .permitAll()
                 .anyRequest()
                 .authenticated();
